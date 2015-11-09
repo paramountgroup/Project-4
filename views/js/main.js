528f88dd574c020b6edfe48e03af2e29292d5e28
Bob <bob@theparamountgroup.us>
2015-11-09 16:57:14 -0500

    start again

--- views/js/main.js
+++ views/js/main.js
@@ -420,7 +420,7 @@ var resizePizzas = function(size) {
   }
 
   changeSliderLabel(size);
-
+/*
   // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
   function determineDx (elem, size) {
     var oldwidth = elem.offsetWidth;
@@ -448,7 +448,7 @@ var resizePizzas = function(size) {
     return dx;
   }
 
-/*
+
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
